f9c1fa00f2dd54eb0422a1f5725647d27fa949f6
khoihn <khoihn@datascience.com.vn>
2020-08-27 17:02:01 +0700

    create structure

---
+++ src/main/java/com/springboot/app/entity/Employee.java
@@ -0,0 +1,61 @@
+package com.springboot.app.entity;
+
+public class Employee {
+    private int employeeId;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+
+    private String first_name;
+    private String last_name;
+    private int salary;
+    private String department;
+    private int managerId;
+
+
+}
