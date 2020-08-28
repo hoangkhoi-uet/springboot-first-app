9610cfa01fae3ecca7ce01f44c34376fcb4fdb16
khoihn <khoihn@datascience.com.vn>
2020-08-28 17:02:45 +0700

    test structure

--- src/main/java/com/springboot/app/entity/Employee.java
+++ src/main/java/com/springboot/app/entity/Employee.java
@@ -2,6 +2,11 @@ package com.springboot.app.entity;
 
 public class Employee {
     private int employeeId;
+    private String firstName;
+    private String lastName;
+    private int salary;
+    private String department;
+    private int managerId;
 
     public int getEmployeeId() {
         return employeeId;
@@ -12,19 +17,19 @@ public class Employee {
     }
 
     public String getFirst_name() {
-        return first_name;
+        return firstName;
     }
 
     public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+        this.firstName = first_name;
     }
 
     public String getLast_name() {
-        return last_name;
+        return lastName;
     }
 
     public void setLast_name(String last_name) {
-        this.last_name = last_name;
+        this.lastName = last_name;
     }
 
     public int getSalary() {
@@ -50,12 +55,5 @@ public class Employee {
     public void setManagerId(int managerId) {
         this.managerId = managerId;
     }
-
-    private String first_name;
-    private String last_name;
-    private int salary;
-    private String department;
-    private int managerId;
-
 
 }
