b9802a88d331f55fb7b3845631c299c3b87046c4
khoihn <khoihn@datascience.com.vn>
2020-09-01 11:59:21 +0700

    create view

---
+++ src/main/java/com/springboot/app/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.app.repository;
+
+import com.springboot.app.entity.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+}
